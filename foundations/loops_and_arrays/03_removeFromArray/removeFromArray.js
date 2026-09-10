cabc3d9f955c043c167eb60cfafaaaf07ca6e077
Raymond <raymond.vuong@torontomu.ca>
2026-09-10 12:30:42 -0400

    Complete 03_removeFromArray

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    return arr.filter(element => {
+        return !nums.includes(element)
+    })
 };
 
 // Do not edit below this line
