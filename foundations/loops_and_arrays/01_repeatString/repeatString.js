a29ce3a7502b4eb9143a26fde01fabc2b1936244
Raymond <raymond.vuong@torontomu.ca>
2026-09-10 12:03:09 -0400

    Complete 01_repeatString

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    newstr = ''
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++) {
+        newstr += str
+    }
+    return newstr
 };
 
 // Do not edit below this line
